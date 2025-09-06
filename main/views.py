87787f4a029a5d593f8d008c6e098f00605f65ab
Fidan <fidankhalilsi24@gmail.com>
2025-09-06 16:05:03 +0700

    Menyelesaikan Tugas 2

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'nama-aplikasi' : 'Football Shop',
+        'namaApp' : 'Football Shop',
         'nama': 'Haru Urara',
         'kelas': 'PBP A'
     }

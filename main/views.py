301808c9148d3dd5f364be14f7fee0ef17e16291
Fidan <fidankhalilsi24@gmail.com>
2025-10-07 10:06:47 +0700

    Menyelesaikan Tugas 4

--- main/views.py
+++ main/views.py
@@ -92,20 +92,20 @@ def register(request):
     return render(request, 'register.html', context)
 
 def login_user(request):
-   if request.method == 'POST':
+    if request.method == 'POST':
       form = AuthenticationForm(data=request.POST)
 
       if form.is_valid():
-        user = form.get_user()
-        login(request, user)
-        response = HttpResponseRedirect(reverse("main:show_main"))
-        response.set_cookie('last_login', str(datetime.datetime.now()))
-        return response
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
 
-   else:
+    else:
         form = AuthenticationForm(request)
-        context = {'form': form}
-        return render(request, 'login.html', context)
+    context = {'form': form}
+    return render(request, 'login.html', context)
 
 def logout_user(request):
     logout(request)
